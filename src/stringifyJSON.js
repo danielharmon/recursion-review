024d45fd9a8de5e53c92055db9041037053e5eb3
Danny Harmon <dannyaharmon@gmail.com>
2020-08-26 19:51:00 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -19,18 +19,22 @@ var stringifyJSON = function(obj) {
     if (result.length > 2) {
       return result.slice(0, -1) + ']';
     } else { return result + ']'; }
-  } if (typeof(obj) === 'object') {
+  } else if (typeof(obj) === 'object') {
     var result = '{';
     var objectLength = Object.keys(obj).length - 1;
     var count = 0;
     for (let keys in obj) {
-      if (count < objectLength) {
-        result += quotes + keys + quotes + ':' + stringifyJSON(obj[keys]) + ',';
-      } else {
-        result += quotes + keys + quotes + ':' + stringifyJSON(obj[keys]);
+      if (typeof(obj[keys]) !== 'function' && obj[keys] !== undefined) {
+        if (count < objectLength) {
+          result += quotes + keys + quotes + ':' + stringifyJSON(obj[keys]) + ',';
+        } else {
+          result += quotes + keys + quotes + ':' + stringifyJSON(obj[keys]);
+        }
+        count++;
       }
-      count++;
     }
     return result + '}';
+  } else if (typeof(obj) === 'function') {
+    return '{}';
   }
 };
